43922fca8eb4be7c1140c67bd21ab5d8d53e63c8
Brady Sutton <bsutt123@gmail.com>
2018-02-10 15:15:42 -0500

    imported bootstrap-sass javascript and removed plugin information from webpack

--- webpack.config.js
+++ webpack.config.js
@@ -18,12 +18,5 @@ module.exports = {
         }
       }
     ]
-  },
-  plugins: [
-    new webpack.ProvidePlugin({
-      $: "jquery",
-      jQuery: "jquery",
-      "window.jQuery": "jquery"
-    })
-  ]
+  }
 }
