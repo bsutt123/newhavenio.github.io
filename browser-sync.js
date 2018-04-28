84c3a13105f0f3aba3905b9cbfe8676e35504c32
Brady Sutton <bsutt123@gmail.com>
2018-04-28 09:34:54 -0400

    creates a simple browser sync file for hot reloading

---
+++ browser-sync.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "server": "_site",
+  "port": "8080",
+  "files": "_site",
+  "open": false,
+  "notify": false,
+  "serveStatic": [
+    '_site'
+  ],
+  "serveStaticOptions": {
+    "extensions": ['html']
+  }
+}
