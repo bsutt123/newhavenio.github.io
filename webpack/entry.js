13679a0a8a1ee58e3203d0319d2c29fdeab3a11a
Brady Sutton <bsutt123@gmail.com>
2018-04-28 09:35:37 -0400

    import SFC and load it into the page when the document is ready

--- webpack/entry.js
+++ webpack/entry.js
@@ -1,5 +1,17 @@
 
 window.$ = require('jquery');
 window.Mustache = require('Mustache');
+import Vue from 'vue';
 
 import 'bootstrap-sass/assets/javascripts/bootstrap.js';
+
+import MeetupEvents from '../js/components/vue/meetupEventsVue.vue';
+
+$(document).ready(function() {
+  var eventsApp = new Vue({
+    el: "#meetup-events",
+    render: function(h) {
+      return h(MeetupEvents);
+    },
+  })
+})
