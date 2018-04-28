5399783d3bf5b597ae9058ae4c0fdc8ac70dff7c
Brady Sutton <bsutt123@gmail.com>
2018-04-28 09:34:09 -0400

    extract utilities into a node exports js file

---
+++ js/components/utilities/component-utils.js
@@ -0,0 +1,12 @@
+function parameterize(params) {
+  return Object.keys(params)
+    .map(function (key) {
+      return encodeURIComponent(key) + '=' +
+        encodeURIComponent(params[key]);
+    })
+    .join('&');
+};
+
+module.exports = {
+  parameterize,
+}
